8fe77116da9636cc623f67673e4a4a9cbe845a8a
Wilkor <wilkorguitarrista@msn.com>
2023-08-30 16:45:49 -0300

    Update functions-chat.js

--- functions-chat.js
+++ functions-chat.js
@@ -16,7 +16,7 @@ document.getElementById('room').value = roomFromURL;
 document.getElementById('name').value = nameFromURL;
 
 if (roomFromURL && nameFromURL) {
-    openChat(roomFromURL, nameFromURL, getRandomColor());
+  openChat(roomFromURL, nameFromURL, getRandomColor());
 }
 
 
@@ -28,65 +28,65 @@ let typingTimeout;
 
 messageInput.addEventListener('input', () => {
 
-    if (!isTyping) {
-        isTyping = true;
-        userTypingSocket(nameFromURL, true);
-    }
+  if (!isTyping) {
+    isTyping = true;
+    userTypingSocket(nameFromURL, true);
+  }
 
-    clearTimeout(typingTimeout);
-    typingTimeout = setTimeout(() => {
-        isTyping = false;
-        userTypingSocket(nameFromURL, false);
-    }, typingIndicatorTimeout);
+  clearTimeout(typingTimeout);
+  typingTimeout = setTimeout(() => {
+    isTyping = false;
+    userTypingSocket(nameFromURL, false);
+  }, typingIndicatorTimeout);
 });
 
 
 document.getElementById('submit-button').addEventListener('click', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const timestampOptions = {
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        day: 'numeric',
-        month: 'numeric',
-        year: 'numeric',
-        timeZone: 'America/Sao_Paulo'
-    };
-    const timestamp = new Date().toLocaleString('pt-BR', timestampOptions);
+  const timestampOptions = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+    timeZone: 'America/Sao_Paulo'
+  };
+  const timestamp = new Date().toLocaleString('pt-BR', timestampOptions);
 
-    const message = {
-        message: messageInput.value,
-        timestamp,
-        bubbleColor: getRandomColor()
-    }
+  const message = {
+    message: messageInput.value,
+    timestamp,
+    bubbleColor: getRandomColor()
+  }
 
 
 
-    if (message.message.trim() !== '') {
-        sendMessageSocket(message);
-        messageInput.value = '';
-    }
+  if (message.message.trim() !== '') {
+    sendMessageSocket(message);
+    messageInput.value = '';
+  }
 });
 
 document.getElementById('join-button').addEventListener('click', () => {
-    const room = document.getElementById('room').value;
-    const name = document.getElementById('name').value;
+  const room = document.getElementById('room').value;
+  const name = document.getElementById('name').value;
 
-    if (room.trim() !== '' && name.trim() !== '') {
-        openChat(room, name);
-    }
+  if (room.trim() !== '' && name.trim() !== '') {
+    openChat(room, name);
+  }
 });
 
 function openChat(room, name, color) {
 
-    joinRoomSocket(room, name, color);
+  joinRoomSocket(room, name, color);
 
-    addSystemMessage(`${name} entrou na sala.`);
+  addSystemMessage(`${name} entrou na sala.`);
 
-    document.querySelector('.input-container').style.display = 'none';
-    document.getElementById('chat').style.display = 'block';
-    document.getElementById('current-room').textContent = room;
+  document.querySelector('.input-container').style.display = 'none';
+  document.getElementById('chat').style.display = 'block';
+  document.getElementById('current-room').textContent = room;
 }
 
 
@@ -98,19 +98,21 @@ function addMessageToChat(user, text, timestamp, bubbleColor) {
   const isOwnMessage = user.toLowerCase() === nameFromURL.toLowerCase();
   messageContainer.classList.add(isOwnMessage ? 'own' : 'other'); // Adiciona a classe 'own' ou 'other'
 
-  if (!isOwnMessage) {
-      const userNameElement = document.createElement('div');
-      userNameElement.textContent = getInitials(user);
-      userNameElement.classList.add('user-initials');
-      userNameElement.setAttribute('title', user);
-      userNameElement.style.backgroundColor = bubbleColor;
-      messageContainer.appendChild(userNameElement);
-  }
 
   const messageElement = document.createElement('div');
   messageElement.textContent = text;
   messageElement.classList.add('message');
   messageContainer.appendChild(messageElement);
+
+  if (!isOwnMessage) {
+    const userNameElement = document.createElement('div');
+    userNameElement.textContent = getInitials(user);
+    userNameElement.classList.add('user-initials');
+    userNameElement.setAttribute('title', user);
+    userNameElement.style.backgroundColor = bubbleColor;
+    messageContainer.appendChild(userNameElement);
+  }
+
 
   const timestampElement = document.createElement('div');
   timestampElement.textContent = timestamp;
@@ -127,107 +129,106 @@ function addMessageToChat(user, text, timestamp, bubbleColor) {
 
 function addSystemMessage(message) {
 
-    if (message.includes('📍')) {
+  if (message.includes('📍')) {
 
-        const messageElement = document.createElement('div');
-        messageElement.textContent = message;
-        messageElement.classList.add('user-name-center');
+    const messageElement = document.createElement('div');
+    messageElement.textContent = message;
+    messageElement.classList.add('user-name-center');
 
-        messagesElement.appendChild(messageElement);
-        messagesElement.scrollTop = messagesElement.scrollHeight;
-    }
+    messagesElement.appendChild(messageElement);
+    messagesElement.scrollTop = messagesElement.scrollHeight;
+  }
 }
 
 
 
 function getInitials(name) {
-    const words = name.split(' ');
-    if (words.length >= 2) {
-        return (words[0][0] + words[1][0]).toUpperCase();
-    } else if (words.length === 1) {
-        return words[0].slice(0, 2).toUpperCase();
-    }
-    return '';
+  const words = name.split(' ');
+  if (words.length >= 2) {
+    return (words[0][0] + words[1][0]).toUpperCase();
+  } else if (words.length === 1) {
+    return words[0].slice(0, 2).toUpperCase();
+  }
+  return '';
 }
 
 function getRandomColor() {
-    const colors = ['#9a53ff', "#d50000", "#4dcb7b"];
-    return colors[Math.floor(Math.random() * colors.length)];
+  const colors = ['#9a53ff', "#d50000", "#4dcb7b"];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
 
 function sendAlert(message, color, delay) {
 
-    Toastify({
-      text: message,
-      duration: delay,
-      newWindow: true,
-      close: true,
-      gravity: "top",
-      position: "center",
-      stopOnFocus: true,
-      style: {
-        background: color,
-      }
-    }).showToast();
-  
-  }
+  Toastify({
+    text: message,
+    duration: delay,
+    newWindow: true,
+    close: true,
+    gravity: "top",
+    position: "center",
+    stopOnFocus: true,
+    style: {
+      background: color,
+    }
+  }).showToast();
 
-  const userListButton = document.getElementById('user-list-button');
-  const userListPopup = document.getElementById('user-list-popup');
-  const closePopupButton = document.getElementById('close-popup-button');
-  const userList = document.getElementById('user-list');
-  const searchUserInput = document.getElementById('search-user'); // Campo de busca
-  const messageInputSearch = document.getElementById('message-input'); // Campo de mensagem
-  
-  // Função para abrir o popup da lista de usuários
-  const openUserListPopup = () => {
-    // Preencha a lista de usuários com os nomes dos usuários da sala
-    console.log(roomListNames)
-    userList.innerHTML = roomListNames.map(user => {
-      if (user.name !== nameFromURL.toLowerCase()) {
-        console.log(user.color)
-        return `
+}
+
+const userListButton = document.getElementById('user-list-button');
+const userListPopup = document.getElementById('user-list-popup');
+const closePopupButton = document.getElementById('close-popup-button');
+const userList = document.getElementById('user-list');
+const searchUserInput = document.getElementById('search-user'); // Campo de busca
+const messageInputSearch = document.getElementById('message-input'); // Campo de mensagem
+
+// Função para abrir o popup da lista de usuários
+const openUserListPopup = () => {
+  // Preencha a lista de usuários com os nomes dos usuários da sala
+  console.log(roomListNames)
+  userList.innerHTML = roomListNames.map(user => {
+    if (user.name !== nameFromURL.toLowerCase()) {
+      console.log(user.color)
+      return `
           <li class="user-item">
             <span class="user-initials-popup" style="background-color: ${user.color};width: 10px;height: 10px;"></span>
             <span class="user-name">${user.name}</span>
           </li>`;
-      }
-    }).join('');
-    userListPopup.style.display = 'block';
-  };
-  
-  userListButton.addEventListener('click', openUserListPopup);
-  
-  closePopupButton.addEventListener('click', () => {
-    userListPopup.style.display = 'none';
-  });
-  
-  document.getElementById('chat-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-  });
-  
+    }
+  }).join('');
+  userListPopup.style.display = 'block';
+};
 
-  
-  // Adiciona evento de entrada de texto para filtrar a lista de usuários
-  searchUserInput.addEventListener('input', () => {
-    const searchTerm = searchUserInput.value.toLowerCase();
-    const filteredUsers = roomListNames.filter(user => user.name.toLowerCase().includes(searchTerm) );
-    userList.innerHTML = filteredUsers.map(user => {
-      return `
+userListButton.addEventListener('click', openUserListPopup);
+
+closePopupButton.addEventListener('click', () => {
+  userListPopup.style.display = 'none';
+});
+
+document.getElementById('chat-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+
+
+// Adiciona evento de entrada de texto para filtrar a lista de usuários
+searchUserInput.addEventListener('input', () => {
+  const searchTerm = searchUserInput.value.toLowerCase();
+  const filteredUsers = roomListNames.filter(user => user.name.toLowerCase().includes(searchTerm));
+  userList.innerHTML = filteredUsers.map(user => {
+    return `
         <li class="user-item">
         <span class="user-initials-popup" style="background-color: ${user.color};width: 10px;height: 10px;"></span>
           <span class="user-name">${user.name}</span>
         </li>`;
-    }).join('');
-  });
-  
-  // Delegação de eventos para tratar cliques nos nomes de usuários
-  userList.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.classList.contains('user-name')) {
-      const userName = target.textContent;
-      messageInputSearch.value = `${messageInputSearch.value.trim()} @${userName}, `;
-      userListPopup.style.display = 'none';
-    }
-  });
-  
+  }).join('');
+});
+
+// Delegação de eventos para tratar cliques nos nomes de usuários
+userList.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.classList.contains('user-name')) {
+    const userName = target.textContent;
+    messageInputSearch.value = `${messageInputSearch.value.trim()} @${userName}, `;
+    userListPopup.style.display = 'none';
+  }
+});
