89004ef2479e5348d7140db8ea19dab7379fd815
Wilkor <wilkorguitarrista@msn.com>
2023-09-08 19:38:16 -0300

    Update socket-room.js

--- socket-room.js
+++ socket-room.js
@@ -80,7 +80,7 @@ socket.on('getRoomData', (data) => {
         console.log(roomByName)
 
         const iframeElement = document.createElement('iframe');
-        iframeElement.src = `http://127.0.0.1:5500/chat.html?name=${nameFromURL}&room=${roomByName}`;
+        iframeElement.src = `https://wilkor.github.io/chat-parse-blip/chat.html?name=${nameFromURL}&room=${roomByName}`;
         iframeElement.classList.add('your-iframe-class');
         iframeElement.style.width = '430px';
         iframeElement.style.height = '700px';
