d3c82d214f43151d39b33f0ad79184679fc56b28
Wilkor <wilkorguitarrista@msn.com>
2023-10-09 13:25:52 -0300

    melhorias
    Transformando chat em PWA

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/chat.html',
+          '/estilo-chat.css',
+          '/socket-chat.js',
+          '/function-chat.js',
+          
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
